8c051f195b3f1f1a093805f1bbf5afc95bf1ca11
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-09-03 17:42:13 +0200

    black

--- mace/cli/visualise_train.py
+++ mace/cli/visualise_train.py
@@ -583,10 +583,16 @@ class InferenceMetric(Metric):
             self.pred_energies_per_atom.append(output["energy"] / atoms_per_config)
 
             self.n_energy += filter_nonzero_weight(
-                batch, self.ref_energies, batch.weight, batch.energy_weight,
+                batch,
+                self.ref_energies,
+                batch.weight,
+                batch.energy_weight,
             )
             filter_nonzero_weight(
-                batch, self.pred_energies, batch.weight, batch.energy_weight,
+                batch,
+                self.pred_energies,
+                batch.weight,
+                batch.energy_weight,
             )
             filter_nonzero_weight(
                 batch,
@@ -646,10 +652,18 @@ class InferenceMetric(Metric):
             self.pred_forces.append(output["forces"])
 
             self.n_forces += filter_nonzero_weight(
-                batch, self.ref_forces, batch.weight, batch.forces_weight, spread_atoms=True,
+                batch,
+                self.ref_forces,
+                batch.weight,
+                batch.forces_weight,
+                spread_atoms=True,
             )
             filter_nonzero_weight(
-                batch, self.pred_forces, batch.weight, batch.forces_weight, spread_atoms=True,
+                batch,
+                self.pred_forces,
+                batch.weight,
+                batch.forces_weight,
+                spread_atoms=True,
             )
 
         # Stress
@@ -658,10 +672,16 @@ class InferenceMetric(Metric):
             self.pred_stress.append(output["stress"])
 
             self.n_stress += filter_nonzero_weight(
-                batch, self.ref_stress, batch.weight, batch.stress_weight,
+                batch,
+                self.ref_stress,
+                batch.weight,
+                batch.stress_weight,
             )
             filter_nonzero_weight(
-                batch, self.pred_stress, batch.weight, batch.stress_weight,
+                batch,
+                self.pred_stress,
+                batch.weight,
+                batch.stress_weight,
             )
 
         # Virials
@@ -674,10 +694,16 @@ class InferenceMetric(Metric):
             self.pred_virials_per_atom.append(output["virials"] / atoms_per_config_3d)
 
             self.n_virials += filter_nonzero_weight(
-                batch, self.ref_virials, batch.weight, batch.virials_weight,
+                batch,
+                self.ref_virials,
+                batch.weight,
+                batch.virials_weight,
             )
             filter_nonzero_weight(
-                batch, self.pred_virials, batch.weight, batch.virials_weight,
+                batch,
+                self.pred_virials,
+                batch.weight,
+                batch.virials_weight,
             )
             filter_nonzero_weight(
                 batch,
--- tests/test_run_train.py
+++ tests/test_run_train.py
@@ -47,11 +47,10 @@ def fixture_fitting_configs():
         c.info["REF_energy"] = np.random.normal(0.1)
         print(c.info["REF_energy"])
         c.new_array("REF_forces", np.random.normal(0.1, size=c.positions.shape))
-        c.info["REF_stress"] = np.random.normal(0.1, size=6)        
+        c.info["REF_stress"] = np.random.normal(0.1, size=6)
         fit_configs.append(c)
 
     return fit_configs
-
 
 
 @pytest.fixture(name="pretraining_configs")
@@ -79,6 +78,7 @@ def fixture_pretraining_configs():
     configs[-1].info["REF_energy"] = -4.0
     configs[-1].info["config_type"] = "IsolatedAtom"
     return configs
+
 
 _mace_params = {
     "name": "MACE",
