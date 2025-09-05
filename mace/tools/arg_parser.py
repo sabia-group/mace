ded34c5a1a41c3fc7c537cb435be6aaecb4cd1ae
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-09-05 13:17:56 +0200

    restored

--- mace/tools/arg_parser.py
+++ mace/tools/arg_parser.py
@@ -955,7 +955,7 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
         "--pme_arguments",
         help="String of dictionary containing the arguments for the PME module",
         type=str,
-        default={},
+        default=None,
         required=False,
     )
     return parser
