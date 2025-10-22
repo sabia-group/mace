2b1aa6036eefd6ffc55433764aa71b029fe20eeb
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-10-22 21:41:22 +0200

    all fixed

--- mace/tools/arg_parser.py
+++ mace/tools/arg_parser.py
@@ -652,6 +652,7 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
             "virials",
             "stress",
             "dipole",
+            "stress+dipole",
             "dipole_polar",
             "huber",
             "universal",
