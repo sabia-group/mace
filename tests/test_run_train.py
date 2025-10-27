75e6dfaf3b96688b391ea334e2fde88c3ed92f32
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-10-27 11:13:58 +0100

    black

--- tests/test_run_train.py
+++ tests/test_run_train.py
@@ -22,14 +22,17 @@ except ImportError:
 
 run_train = Path(__file__).parent.parent / "mace" / "cli" / "run_train.py"
 
+
 # ----------------------------- #
 @pytest.fixture(name="fitting_configs_nan")
 def fixture_fitting_configs_nan():
     return _fitting_config(with_nan=True)
 
+
 @pytest.fixture(name="fitting_configs")
 def fixture_fitting_configs():
     return _fitting_config(with_nan=False)
+
 
 def _fitting_config(with_nan: bool = False):
     water = Atoms(
@@ -46,14 +49,14 @@ def _fitting_config(with_nan: bool = False):
     fit_configs[0].info["config_type"] = "IsolatedAtom"
     fit_configs[1].info["REF_energy"] = 0.0
     fit_configs[1].info["config_type"] = "IsolatedAtom"
-    
+
     if with_nan:
         np.random.seed(0)
         c = water.copy()
         c.positions += np.random.normal(0.1, size=c.positions.shape)
         c.info["REF_energy"] = np.nan
-        c.new_array("REF_forces", np.full(c.positions.shape,np.nan))
-        c.info["REF_stress"] = np.full(6,np.nan)
+        c.new_array("REF_forces", np.full(c.positions.shape, np.nan))
+        c.info["REF_stress"] = np.full(6, np.nan)
         fit_configs.append(c)
 
     np.random.seed(5)
@@ -67,6 +70,7 @@ def _fitting_config(with_nan: bool = False):
         fit_configs.append(c)
 
     return fit_configs
+
 
 # ----------------------------- #
 @pytest.fixture(name="pretraining_configs")
@@ -123,14 +127,17 @@ _mace_params = {
     "use_reduced_cg": False,
 }
 
-# ----------------------------- # 
+
+# ----------------------------- #
 def test_run_train(tmp_path, fitting_configs):
     _run_train(tmp_path, fitting_configs, compare=True)
+
 
 def test_run_train_nan(tmp_path, fitting_configs_nan):
     _run_train(tmp_path, fitting_configs_nan, compare=False)
 
-def _run_train(tmp_path:str, configs:str,compare:bool):
+
+def _run_train(tmp_path: str, configs: str, compare: bool):
     ase.io.write(tmp_path / "fit.xyz", configs)
 
     mace_params = _mace_params.copy()
@@ -163,15 +170,15 @@ def _run_train(tmp_path:str, configs:str,compare:bool):
     calc = MACECalculator(model_paths=tmp_path / "MACE.model", device="cpu")
 
     Es = []
-    for at in configs: # [:2] + fitting_configs[3:]:
+    for at in configs:  # [:2] + fitting_configs[3:]:
         at.calc = calc
         Es.append(at.get_potential_energy())
 
     print("Es", Es)
-    
+
     if not compare:
         return
-    
+
     # from a run on 04/06/2024 on stress_bugfix 967f0bfb6490086599da247874b24595d149caa7
     ref_Es = [
         0.0,
@@ -199,15 +206,18 @@ def _run_train(tmp_path:str, configs:str,compare:bool):
     ]
 
     assert np.allclose(Es, ref_Es)
- 
-# ----------------------------- #   
+
+
+# ----------------------------- #
 def test_run_train_missing_data(tmp_path, fitting_configs):
-    _run_train_missing_data(tmp_path,fitting_configs,True)
-    
+    _run_train_missing_data(tmp_path, fitting_configs, True)
+
+
 def test_run_train_missing_data_nan(tmp_path, fitting_configs_nan):
-    _run_train_missing_data(tmp_path,fitting_configs_nan,False)
-    
-def _run_train_missing_data(tmp_path, configs,compare):
+    _run_train_missing_data(tmp_path, fitting_configs_nan, False)
+
+
+def _run_train_missing_data(tmp_path, configs, compare):
     del configs[5].info["REF_energy"]
     del configs[6].arrays["REF_forces"]
     del configs[7].info["REF_stress"]
@@ -247,7 +257,7 @@ def _run_train_missing_data(tmp_path, configs,compare):
     for at in configs:
         at.calc = calc
         Es.append(at.get_potential_energy())
-        
+
     if not compare:
         return
 
@@ -279,13 +289,17 @@ def _run_train_missing_data(tmp_path, configs,compare):
     ]
     assert np.allclose(Es, ref_Es)
 
+
 # ----------------------------- #
 def test_run_train_no_stress(tmp_path, fitting_configs):
-    _run_train_no_stress(tmp_path, fitting_configs,True)
+    _run_train_no_stress(tmp_path, fitting_configs, True)
+
+
 def test_run_train_no_stress_nan(tmp_path, fitting_configs_nan):
-    _run_train_no_stress(tmp_path, fitting_configs_nan,False)
-    
-def _run_train_no_stress(tmp_path, configs,compare):
+    _run_train_no_stress(tmp_path, fitting_configs_nan, False)
+
+
+def _run_train_no_stress(tmp_path, configs, compare):
     del configs[5].info["REF_energy"]
     del configs[6].arrays["REF_forces"]
     del configs[7].info["REF_stress"]
@@ -326,7 +340,7 @@ def _run_train_no_stress(tmp_path, configs,compare):
     for at in configs:
         at.calc = calc
         Es.append(at.get_potential_energy())
-        
+
     if not compare:
         return
 
@@ -357,6 +371,7 @@ def _run_train_no_stress(tmp_path, configs,compare):
         -0.1489441490225184,
     ]
     assert np.allclose(Es, ref_Es)
+
 
 # ----------------------------- #
 def test_run_train_multihead(tmp_path, fitting_configs):
@@ -469,6 +484,7 @@ def test_run_train_multihead(tmp_path, fitting_configs):
     ]
     assert np.allclose(Es, ref_Es)
 
+
 # ----------------------------- #
 def test_run_train_foundation(tmp_path, fitting_configs):
     ase.io.write(tmp_path / "fit.xyz", fitting_configs)
@@ -542,6 +558,7 @@ def test_run_train_foundation(tmp_path, fitting_configs):
         0.7301387786865234,
     ]
     assert np.allclose(Es, ref_Es)
+
 
 # ----------------------------- #
 def test_run_train_foundation_multihead(tmp_path, fitting_configs):
@@ -670,6 +687,7 @@ def test_run_train_foundation_multihead(tmp_path, fitting_configs):
         0.5574042201042175,
     ]
     assert np.allclose(Es, ref_Es, atol=1e-1)
+
 
 # ----------------------------- #
 def test_run_train_foundation_multihead_json(tmp_path, fitting_configs):
@@ -807,6 +825,7 @@ def test_run_train_foundation_multihead_json(tmp_path, fitting_configs):
         0.5574042201042175,
     ]
     assert np.allclose(Es, ref_Es, atol=1e-1)
+
 
 # ----------------------------- #
 def test_run_train_multihead_replay_custom_finetuning(
@@ -1951,6 +1970,7 @@ def test_run_train_real_pt_data_ratio(
     ]
     assert len(l_ratio) == 1
     assert l_ratio[0].strip().endswith(" 1")
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
