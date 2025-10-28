3ef89f6010aa1f619aa771893d82afdfc2c630ce
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-10-28 11:04:31 +0100

    silent warning

--- mace/__init__.py
+++ mace/__init__.py
@@ -1,5 +1,10 @@
 import os
 
 from .__version__ import __version__
+import warnings
+
+warnings.filterwarnings(
+    "ignore", category=UserWarning, message=".*TorchScript type system.*"
+)
 
 os.environ["TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD"] = "1"
