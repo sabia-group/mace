bc29658c69b60d65a4c2382ff18c0b896a6a927c
Elia Stocco <elia.stocco@mpsd.mpg.de>
2026-02-11 15:00:26 +0100

    black

--- mace/tools/train.py
+++ mace/tools/train.py
@@ -549,6 +549,7 @@ def take_step_lbfgs(
 
     return loss, loss_dict
 
+
 # Keep parameters frozen/active after evaluation
 @contextmanager
 def preserve_grad_state(model):
@@ -564,6 +565,7 @@ def preserve_grad_state(model):
         for param, requires_grad in requires_grad_backup.items():
             param.requires_grad = requires_grad
 
+
 def evaluate(
     model: torch.nn.Module,
     loss_fn: torch.nn.Module,
@@ -571,7 +573,6 @@ def evaluate(
     output_args: Dict[str, bool],
     device: torch.device,
 ) -> Tuple[float, Dict[str, Any]]:
-
 
     metrics = MACELoss(loss_fn=loss_fn).to(device)
 
